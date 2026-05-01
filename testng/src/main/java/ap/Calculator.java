c9bc108638d53ac4d9c03860a5b3cd63cf86c18d
camferreira <catarina17ferreira2003@gmail.com>
2026-05-01 19:49:55 +0100

    Fix: handle null values correctly in divide method of Calculator

--- testng/src/main/java/ap/Calculator.java
+++ testng/src/main/java/ap/Calculator.java
@@ -51,12 +51,14 @@ public class Calculator {
      * @throws IllegalArgumentException if b is 0.
       **/
     public Integer divide(Integer a, Integer b) {
-        int x = (a==null) ? 0 : a;
         if (b==null || b == 0) throw new IllegalArgumentException();
         
         ++numberOfOperations;
         
-        return x / b;
+        if (a == null)
+            return 0; 
+
+        return a / b;
     }
 
     /**
