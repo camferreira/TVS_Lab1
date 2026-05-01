e2b98d51289996312d4a744d7fae3a660503b35e
camferreira <catarina17ferreira2003@gmail.com>
2026-05-01 19:05:10 +0100

    Add: unit tests for Calculator constructor validation

--- testng/src/test/java/ap/TestCalculator.java
+++ testng/src/test/java/ap/TestCalculator.java
@@ -8,5 +8,25 @@ import static org.testng.Assert.*;
 
 @Test
 public class TestCalculator {
+    @Test
+    public void testConstructorValidName() {
+        Calculator c = new Calculator("Calc");
+        assertEquals(c.getName(), "Calc");
+        assertEquals(c.getNumberOfOperations(), 0);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testConstructorNullName() {
+        new Calculator(null);
+    }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testConstructorTooShortName() {
+        new Calculator("C");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testConstructorTooLongName() {
+        new Calculator("Calcul");
+    }
 }
